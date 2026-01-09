490aeed63815b9f45ca8bc89be679b9f54f33d61
staticWagomU <to6wa17@gmail.com>
2026-01-09 13:42:28 +0900

    feat: register SetMarkCommand in RegionSelectPlugin
    Sprint 3 Subtask 1 (structural): コマンド統合
    - main.tsにMarkManagerシングルトン追加
    - SetMarkCommandをaddCommandで登録（id: set-mark）
    - Notice依存性を注入してユーザー通知を実現

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
-import { Plugin } from "obsidian";
+import { Notice, Plugin } from "obsidian";
 import { DEFAULT_SETTINGS, RegionSelectSettings, RegionSelectSettingTab } from "./settings";
+import { MarkManager } from "./MarkManager";
+import { SetMarkCommand } from "./SetMarkCommand";
 
 export default class RegionSelectPlugin extends Plugin {
 	settings: RegionSelectSettings;
+	private markManager: MarkManager;
 
 	async onload() {
 		await this.loadSettings();
+
+		this.markManager = new MarkManager();
+
+		this.addCommand({
+			id: "set-mark",
+			name: "Set mark at cursor",
+			editorCallback: (editor) => {
+				const command = new SetMarkCommand(
+					this.markManager,
+					(message: string) => new Notice(message)
+				);
+				command.execute(editor);
+			},
+		});
 
 		this.addSettingTab(new RegionSelectSettingTab(this.app, this));
 	}
